5e57ec1705489716f4441c0b80ed2f2310cc9e56
Ashish Das <ashishdas4real@icloud.com>
2022-07-19 16:31:05 +0530

    Devsearch Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#Django Hands-on
+we are getting hands-on with the basic functionality of **Django**
---
+++ projects_page/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import PROJECT_TABLE
+
+admin.site.register(PROJECT_TABLE)
