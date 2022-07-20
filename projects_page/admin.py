e6489461c7b3fc4e9bb11f5f7932b84edb56c820
Ashish Das <ashishdas4real@icloud.com>
2022-07-20 14:32:38 +0530

    New Models Added.

--- projects_page/admin.py
+++ projects_page/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import PROJECT_TABLE
+from .models import PROJECT_TABLE, REVIEW_TABLE, TAG_TABLE
 
 admin.site.register(PROJECT_TABLE)
+admin.site.register(REVIEW_TABLE)
+admin.site.register(TAG_TABLE)
+
--- projects_page/models.py
+++ projects_page/models.py
@@ -10,5 +10,40 @@ class PROJECT_TABLE(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     
+    tags = models.ManyToManyField('TAG_TABLE', blank=True)
+    # if we had the TAG_TABLE class above the PROJECT_TABLE class we could have put it as
+    # -- tags = models.ManyToManyField(TAG_TABLE)
+    # but since it is below this class we reference it by putting it inside quotes like 'TAG_TABLE'
+    
+    vote_count = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
+    
+    
     def __str__(self):
         return self.title
+    
+class REVIEW_TABLE(models.Model):
+    VOTE_TYPE = (
+        ('up','UPVOTE'),
+        ('down','DOWNVOTE')
+    )
+    #owner=
+    project = models.ForeignKey(PROJECT_TABLE, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    
+    def __str__(self):
+        return self.value
+    
+class TAG_TABLE(models.Model):
+    
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    
+    def __str__(self):
+        return self.name
+    
+    
